64b8833f3d359064ae002291496d00267930a849
Luc <luc.lonqq@gmail.com>
2026-04-17 14:14:40 +0200

    add cors config

--- src/go.mod
+++ src/go.mod
@@ -3,6 +3,7 @@ module score-tracker
 go 1.25.3
 
 require (
+	github.com/gin-contrib/cors v1.7.6
 	github.com/gin-gonic/gin v1.11.0
 	gorm.io/driver/postgres v1.6.0
 	gorm.io/gorm v1.31.1
--- src/main.go
+++ src/main.go
@@ -9,6 +9,7 @@ import (
 	"score-tracker/osuservices"
 	"score-tracker/routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -29,6 +30,12 @@ func main() {
 	}
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowHeaders: []string{"Origin", "Content-Type", "Accept", "Authorization"},
+	}))
 
 	r.Use(middlewares.DatabaseMiddleware(database.DB))
 
